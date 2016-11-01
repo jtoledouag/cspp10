2d719ba5bc660624226385d13f4d96c49593d448
Jessica <jessicatoledo@uagateway.org>
2016-11-01 16:01:11 +0000

    today i finally finished the ATM lab

--- unit3/jtoledo_mockatm.py
+++ unit3/jtoledo_mockatm.py
@@ -3,9 +3,10 @@ print("1. Withdraw \n2. Deposit \n3. Exit")
 choice = input("Welcome to ATM! Pick from above [1|2|3]:")
 while choice != "3":
     if choice == "1": #user chooses 'withdraw'
-       bank_account = bank_account - amount#student does this part
+        amount = int(input("How much to withdraw: "))
+        bank_account = bank_account - amount#student does this part
     elif choice == "2":
-        amount = input("How much to deposit: ")
+        amount = int(input("How much to deposit: "))
         bank_account = bank_account + amount
     elif choice == "3": #user chooses 'exit'
        exit = input ("exit") #student does this part too
