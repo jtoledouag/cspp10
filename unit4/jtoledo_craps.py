f3535f543144c81ac0dda8a4c96290311f3850c5
Jessica <jessicatoledo@uagateway.org>
2016-12-23 17:13:14 +0000

    i worked on my first_roll function

--- unit4/jtoledo_craps.py
+++ unit4/jtoledo_craps.py
@@ -17,15 +17,39 @@ def roll2dice():
     print("Rolled 2 dice: {} {}".format(dice1,dice2))
     return dice_sum
 
-roll2dice()
+def get_bet(bank_amount):
+    bet = int(input("How much would you like to bet?"))
+    while True: 
+        if bet < 0:
+             print ("There's no negative numbers allowed!")
+        elif bet <= bank_amount:
+            return bet
+        elif bet > 100:
+            print("You don't have the money!")
 
- 
+def first_roll(player_sum,point_roll):
+    if player_sum == 2 or 3 or 12:
+        return "player loses"
+    elif player_sum == 7 or 11:
+        
+
 
 
 #   function name: update_bank
 #   purpose: change bank amount based on wheather the player won or not
 #   argument: none 
+def craps():
 
+    int(input("how much would you like to bet: "))
+
+    dice1 = random.randint(1,6)
+    dice2 = random.randint(1,6)
+    dice_sum = dice1 + dice2
+    print("Rolled 2 dice: {} {}".format(dice1,dice2))
+    return dice_sum
+
+
+craps()
 
 
 
