917363b0408690ab3e583522299f37ff857a9f24
Jessica <jessicatoledo@uagateway.org>
2017-01-20 17:16:31 +0000

    i worked with Nicole on the extend

--- unit3/jtoledo_numguess.py
+++ unit3/jtoledo_numguess.py
@@ -1,1 +1,23 @@
-io0
+import random
+#computer picks a number
+correct_number = random.randint(1,100)
+# print(correct_number)
+user_guess = 200
+guesses = 0
+
+while user_guess != correct_number:
+    #computer ask player what number did they choose (repeat)
+    user_guess = int(input("pick a number between 1 and 100: "))
+    guesses = guesses + 1
+    #if the player chooses a number greater than the correct number then the computer would tell the player to pick a lower number (repeat)
+    if user_guess > correct_number:
+        print("pick a lower number")
+    #if the player chooses a number less than the correct number then the computer would tell the player to pick a higher number (repeat)
+    if user_guess < correct_number:
+        print("pick a higher number")
+    # if the player chooses the correct number then the computer will tell the player that the player got it correct and the game ends (repeat)
+    if user_guess == correct_number:
+        print("you won!")
+        print("you have guessed {} times".format(guesses))
+
+
--- unit4/mp_list2.py
+++ unit4/mp_list2.py
@@ -1,6 +1,7 @@
 x_list = []
 while True:
     user_input = int(input("enter any number you want"))
+    
 
 
 
